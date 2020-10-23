01c3ea9a99cac5de7540bcf32dfd62521c21748c
vaibhav-singal <vaibhavsingal903@gmail.com>
2020-10-23 20:58:02 +0530

    learnt about vanishing the pigs

--- Slingshot.js
+++ Slingshot.js
@@ -18,6 +18,11 @@ class SlingShot{
         this.sling.bodyA = null;
     }
 
+    attach(body) {
+    this.sling.bodyA = body; 
+    }
+
+
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
